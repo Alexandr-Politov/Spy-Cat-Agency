3d48e2ea3c1c3db603dd3305fe48ce7c4b16ceb6
Alexandr Politov <apolitov83@gmail.com>
2024-11-22 16:53:44 +0200

    service function for breed validation implemented and added to SpyCat serializer

--- agency/serializers.py
+++ agency/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from agency.models import SpyCat, Mission, Target
+from agency.services import is_breed_valid
 
 
 class SpyCatSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpyCat
         fields = ["id", "name", "years_experience", "breed", "salary"]
+
+    def validate_breed(self, breed_name: str) -> str:
+        if not is_breed_valid(breed_name):
+            raise ValidationError(
+                "Invalid breed name."
+                "You should choose a breed from 'api.thecatapi.com/v1/breeds'"
+            )
+        return breed_name
 
 
 class TargetSerializer(serializers.ModelSerializer):
