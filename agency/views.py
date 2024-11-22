cd8728611a6cc27cb603b894c80c44c36b3cbdf5
Alexandr Politov <apolitov83@gmail.com>
2024-11-22 16:25:48 +0200

    base viewsets for SpyCat and Mission created

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from agency.models import SpyCat, Mission
+from agency.serializers import SpyCatSerializer, MissionSerializer
 
-# Create your views here.
+
+class SpyCatViewSet(ModelViewSet):
+    queryset = SpyCat.objects.all()
+    serializer_class = SpyCatSerializer
+
+
+class MissionViewSet(ModelViewSet):
+    queryset = Mission.objects.all()
+    serializer_class = MissionSerializer
